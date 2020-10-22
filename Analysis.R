aeec59811f2de7089057942ec8f61a15f2bbba7f
Poppy Stanton <ps1102@york.ac.uk>
2020-10-22 23:11:41 +0100

    Created data set and summerised it

---
+++ Analysis.R
@@ -0,0 +1,28 @@
+# Create a random data set foe demo 
+# Main data set 
+
+df <- data.frame(x = rnorm(50, 16, 4), group = rep(c("group1", "group2"), each = 25))
+
+# Load tidyverse
+
+library(tidyverse)
+
+# Summerising the data 
+
+df %>% group_by(group) %>% summarise(mean = mean(x), n = length(n), sd = sd(x))
+
+# A tibble: 2 x 4
+# group   mean     n    sd
+# <fct>  <dbl> <int> <dbl>
+#  1 group1  15.7     1  4.77
+#  2 group2  15.7     1  4.62
+
+
+
+
+
+
+
+
+
+
